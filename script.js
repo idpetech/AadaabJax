36fe56e4efb0512f2085338474e51f6efd2d71c8
idpetech <techvizpro@yahoo.com>
2025-12-03 00:21:40 -0500

    Update website content and add new pages

--- script.js
+++ script.js
@@ -18,3 +18,14 @@ async function sendChatMessage(message){
     return {error: true};
   }
 }
+
+function openTab(evt, name){
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for(i=0;i<tabcontent.length;i++) tabcontent[i].style.display="none";
+  tablinks = document.getElementsByClassName("tablink");
+  for(i=0;i<tablinks.length;i++) tablinks[i].className = tablinks[i].className.replace(" active","");
+  document.getElementById(name).style.display="block";
+  evt.currentTarget.className += " active";
+}
+
